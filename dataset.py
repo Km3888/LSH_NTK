335427580a51baa76ecee93ccdb32dd411fcd60d
kellymarshall <km3888@nyu.edu>
2020-12-09 19:50:31 -0500

    Changed dataset to MSD year-prediction

---
+++ dataset.py
@@ -0,0 +1,55 @@
+import torch
+from torch.utils.data import Dataset
+
+class MSDDataset(Dataset):
+    # A pytorch dataset class for holding data.
+    def __init__(self,train=True):
+        '''
+        Takes as input the name of a file containing sentences with a classification label (comma separated) in each line.
+        Stores the text data in a member variable X and labels in y
+        '''
+
+        # Opening the file and storing its contents in a list
+        with open('YearPredictionMSD.txt') as f:
+            lines = f.read().split('\n')
+
+        # Splitting the data and labels from each other
+        X, y = [], []
+        for i,line in enumerate(lines):
+            if (not train and i<463715) or (train and i>=463715) or (not len(line)):
+                continue
+            x_vals=line.split(',')[1:]
+            X.append([float(x) for x in x_vals])
+            y.append(float(line.split(',')[0]))
+
+        # Store them in member variables.
+        self.X = torch.tensor(X)
+        self.y = torch.tensor(y)
+
+        self.X,self.x_avg,self.x_std=self.normalize(self.X)
+        self.y, self.y_avg, self.y_std = self.normalize(self.y)
+
+    def normalize(self,data):
+        avg=data.mean(dim=0)
+        data-=avg
+        std=(data**2).mean(dim=0)
+        std=std**0.5
+        data/=std
+        return data,avg,std
+
+
+    def preprocess(self, data):
+        return data
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, index):
+        return self.preprocess(self.X[index]), self.y[index]
+
+if __name__=='__main__':
+
+    dataset=MSDDataset()
+    train_kwargs = {'batch_size': 16}
+    loader= torch.utils.data.DataLoader(dataset,**train_kwargs)
+    X,y=next(iter(loader))
--- main.py
+++ main.py
@@ -6,31 +6,18 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-
+from dataset import MSDDataset
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(90, 32)
+        self.fc2 = nn.Linear(32, 1)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
+        h_1 = self.fc1(x)
+        h_1 = F.relu(h_1)
+        output = self.fc2(h_1)
         return output
 
 
@@ -40,7 +27,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = F.mse_loss(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
@@ -54,20 +41,16 @@ def train(args, model, device, train_loader, optimizer, epoch):
 def test(model, device, test_loader):
     model.eval()
     test_loss = 0
-    correct = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            test_loss += F.mse_loss(output, target, reduction='sum').item()  # sum up batch loss
 
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    print('\nTest set: Average loss: {:.4f}\n'.format(
+        test_loss))
 
 
 def main():
@@ -109,14 +92,8 @@ def main():
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-        ])
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
-                       transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
-                       transform=transform)
+    dataset1,dataset2=MSDDataset(train=True),MSDDataset(train=False)
+
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
@@ -130,7 +107,7 @@ def main():
         scheduler.step()
 
     if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+        torch.save(model.state_dict(), "MSD_model.pt")
 
 
 if __name__ == '__main__':
