18c345fd68da778082935ecffd5feb29a3257e79
kellymarshall <km3888@nyu.edu>
2020-12-10 19:43:57 -0500

    Functional SGD training for LM and NN

--- main.py
+++ main.py
@@ -4,114 +4,109 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from dataset import MSDDataset
+# import pydevd_pycharm
+from torch.utils.tensorboard import SummaryWriter
+# pydevd_pycharm.settrace('km3888@hegde-lambda-1.engineering.nyu.edu', port=22, stdoutToServer=True, stderrToServer=True)
+
+class LM(nn.Module):
+    def __init__(self):
+        super(LM, self).__init__()
+        self.fc1 = nn.Linear(90, 1)
+
+    def forward(self, x):
+        output = self.fc1(x)
+        return output
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(90, 32)
+        self.dropout= nn.Dropout(p=0.2)
         self.fc2 = nn.Linear(32, 1)
 
     def forward(self, x):
         h_1 = self.fc1(x)
+        h_1 = self.dropout(h_1)
         h_1 = F.relu(h_1)
         output = self.fc2(h_1)
         return output
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(args, model, device, train_loader, optimizer, epoch,writer,test_loader):
     model.train()
-    param_vector=get_param_vector(model)
-
+    # param_vector=get_param_vector(model)
+    train_loss=0
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        first_data=data[0,:,:,:].view([1,1,28,28])
-        grad_vector = get_grad_vector(model,first_data)
+        #first_data=data[0].view([1,90])
+        #grad_vector = get_grad_vector(model,first_data)
         optimizer.zero_grad()
-        output = model(data)
+        output = model(data).view(-1)
         loss = F.mse_loss(output, target)
         loss.backward()
         optimizer.step()
+        train_loss+=loss
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+            curr_iter= (epoch-1)*len(train_loader)+batch_idx
+
+            test_loss=test(model,device,test_loader,num_samples=5000)
+            train_loss=calc_train_loss(model,device,train_loader,num_samples=5000)
+
+            writer.add_scalar('test loss',test_loss,curr_iter)
+            writer.add_scalar('train loss',train_loss,curr_iter)
+
+            train_loss=0
             if args.dry_run:
                 break
+    return train_loss
+
+def calc_train_loss(model, device, train_loader,num_samples=None):
+    model.eval()
+    train_loss = 0
+    if num_samples is None:
+        num_samples=len(train_loader.dataset)
+    with torch.no_grad():
+        for i,(data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            output = model(data).view(-1)
+            indv_loss = F.mse_loss(output, target, reduction='sum').item()
+            train_loss += indv_loss  # sum up batch loss
+            if i>num_samples:
+                break
+
+    train_loss /= num_samples
+
+    return train_loss
+
+def compute_approximation(model,data):
+    pass
 
 
-def test(model, device, test_loader):
+def test(model, device, test_loader,num_samples=None):
     model.eval()
     test_loss = 0
+    if num_samples is None:
+        num_samples = len(test_loader.dataset)
+    total_samples=0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.mse_loss(output, target, reduction='sum').item()  # sum up batch loss
+            output = model(data).view(-1)
+            indv_loss = F.mse_loss(output, target, reduction='sum').item()
+            test_loss += indv_loss  # sum up batch loss
+            total_samples+=data.shape[0]
+            if total_samples>num_samples:
+                break
 
-    test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}\n'.format(
-        test_loss))
+    test_loss /= total_samples
 
-
-def main():
-    # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-
-    torch.manual_seed(args.seed)
-
-    device = torch.device("cuda" if use_cuda else "cpu")
-
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
-    if use_cuda:
-        cuda_kwargs = {'num_workers': 1,
-                       'pin_memory': True,
-                       'shuffle': True}
-        train_kwargs.update(cuda_kwargs)
-        test_kwargs.update(cuda_kwargs)
-
-    dataset1,dataset2=MSDDataset(train=True),MSDDataset(train=False)
-
-    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
-
-    model = Net().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
-        scheduler.step()
-
-    if args.save_model:
-        torch.save(model.state_dict(), "MSD_model.pt")
+    return test_loss
 
 def get_param_vector(model):
     params=[]
@@ -120,7 +115,6 @@ def get_param_vector(model):
     return torch.cat(params)
 
 def get_grad_vector(model,input):
-    #TODO remove .sum() once switched over to 1D output
     output=model(input).sum()
     model.zero_grad()
     output.backward()
@@ -132,6 +126,68 @@ def get_grad_vector(model,input):
 def estimate_output(y_0, w, w0, grad):
     diff = w - w0
     return y_0 + torch.dot(grad, diff)
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=1, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    parser.add_argument('--train_size', type=int, default=463715)
+    parser.add_argument('--test_size', type=int, default=1000)
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    writer=SummaryWriter()
+
+    train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.test_batch_size}
+    if use_cuda:
+        cuda_kwargs = {'num_workers': 0,#TODO change this back to 1
+                       'pin_memory': True,
+                       'shuffle': True}
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+
+    dataset1,dataset2=MSDDataset(train=True),MSDDataset(train=False)
+    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2,**test_kwargs)
+
+    model = Net().to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    train_losses=[]
+    test_losses=[]
+    for epoch in range(1, args.epochs + 1):
+        train_loss=train(args, model, device, train_loader, optimizer, epoch,writer,test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "MSD_model.pt")
+
 
 
 if __name__ == '__main__':
